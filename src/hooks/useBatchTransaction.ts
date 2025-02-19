bc549ceff436817dfd4e32f0472533ccc4332af5
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2025-02-19 13:25:05 -0800

    panic on conversion

--- src/hooks/useBatchTransaction.ts
+++ src/hooks/useBatchTransaction.ts
@@ -61,7 +61,7 @@ transaction(calls: [{String: String}], mustPass: Bool) {
             let valueStr = call["value"]!
             let targetAddr = EVM.addressFromString(addrStr)
             let callData: [UInt8] = dataStr.decodeHex()
-            let valueAttoflow = UInt64.fromString(valueStr)
+            let valueAttoflow = UInt.fromString(valueStr) ?? panic("Could not construct UInt value from ".concat(valueStr))
             let result = self.coa.call(
                 to: targetAddr,
                 data: callData,
